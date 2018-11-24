f6db5c74db9a095596347333a616f81b7846d018
Blackdread <jojo.julien@gmail.com>
2018-11-25 00:13:18 +0800

    Simple API for (shoot) command, for internal use only

--- camera-framework/src/main/java/org/blackdread/cameraframework/util/NameToBeDefined2.java
+++ camera-framework/src/main/java/org/blackdread/cameraframework/util/NameToBeDefined2.java
@@ -7,7 +7,8 @@ import org.blackdread.cameraframework.exception.EdsdkErrorException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -19,93 +20,92 @@ import java.util.function.Function;
  *
  * @author Yoann CAPLAIN
  */
-public class NameToBeDefined implements Runnable {
+public class NameToBeDefined2<V> implements Callable<V> {
 
     private static final int DEFAULT_DELAY_MILLIS = 200;
 
-    private final Runnable runnable;
+    private final Callable<V> callable;
 
-    public static NameToBeDefined wrap(final Runnable runnableToDecorate) {
-        return new NameToBeDefined(runnableToDecorate);
+    public static <V> NameToBeDefined2<V> wrap(final Callable<V> callableToDecorate) {
+        return new NameToBeDefined2<>(callableToDecorate);
     }
 
-    protected NameToBeDefined(Runnable runnableToDecorate) {
-        this.runnable = Objects.requireNonNull(runnableToDecorate);
+    protected NameToBeDefined2(Callable<V> callableToDecorate) {
+        this.callable = callableToDecorate;
     }
 
-    public NameToBeDefined retryOnBusy() {
+    public NameToBeDefined2<V> retryOnBusy() {
         // can be called many times to add many retries
         return retryOnBusy(DEFAULT_DELAY_MILLIS, 0);
     }
 
-    public NameToBeDefined retryOnBusy(final long delayMillis) {
+    public NameToBeDefined2<V> retryOnBusy(final long delayMillis) {
         // can be called many times to add many retries with different delay
         return retryOnBusy(delayMillis, 0);
     }
 
-    public NameToBeDefined retryOnBusy(final long... delayMillis) {
+    public NameToBeDefined2<V> retryOnBusy(final long... delayMillis) {
         // can be called many times to add many retries with different delay
-        NameToBeDefined tmp = this;
+        NameToBeDefined2<V> tmp = this;
         for (final long delayMilli : delayMillis) {
             tmp = retryOnBusy(delayMilli, 0);
         }
         return tmp;
     }
 
-    public NameToBeDefined retryOnBusy(final long delayMillis, final int retryTimes) {
+    public NameToBeDefined2<V> retryOnBusy(final long delayMillis, final int retryTimes) {
         // can be called many times to add many retries with different delay
         if (delayMillis < 0)
             throw new IllegalArgumentException("Delay must be higher than 0");
         if (retryTimes < 0)
             throw new IllegalArgumentException("Retry times must be higher than 0");
-        return new NameToBeDefined(() -> {
+        return new NameToBeDefined2<>(() -> {
             int retry = -1;
             do {
                 try {
-                    this.runnable.run();
-                    return;
+                    return this.callable.call();
                 } catch (EdsdkDeviceBusyErrorException e) {
                     if (sleep(delayMillis))
-                        return;
+                        return null;
                     retry++;
                 }
             } while (retry < retryTimes);
+            return null;
         });
     }
 
-    public NameToBeDefined retryOnError(final long delayMillis, final EdsdkError errors) {
+    public NameToBeDefined2<V> retryOnError(final long delayMillis, final EdsdkError errors) {
         return retryOnError(delayMillis, 0, Collections.singletonList(errors));
     }
 
-    public NameToBeDefined retryOnError(final long delayMillis, final EdsdkError... errors) {
+    public NameToBeDefined2<V> retryOnError(final long delayMillis, final EdsdkError... errors) {
         return retryOnError(delayMillis, 0, Arrays.asList(errors));
     }
 
-    public NameToBeDefined retryOnError(final long delayMillis, final int retryTimes, final EdsdkError errors) {
+    public NameToBeDefined2<V> retryOnError(final long delayMillis, final int retryTimes, final EdsdkError errors) {
         return retryOnError(delayMillis, retryTimes, Collections.singletonList(errors));
     }
 
-    public NameToBeDefined retryOnError(final long delayMillis, final int retryTimes, final EdsdkError... errors) {
+    public NameToBeDefined2<V> retryOnError(final long delayMillis, final int retryTimes, final EdsdkError... errors) {
         return retryOnError(delayMillis, retryTimes, Arrays.asList(errors));
     }
 
-    public NameToBeDefined retryOnError(final long delayMillis, final int retryTimes, final List<EdsdkError> errors) {
+    public NameToBeDefined2<V> retryOnError(final long delayMillis, final int retryTimes, final List<EdsdkError> errors) {
         if (delayMillis < 0)
             throw new IllegalArgumentException("Delay must be higher than 0");
         if (retryTimes < 0)
             throw new IllegalArgumentException("Retry times must be higher than 0");
-        return new NameToBeDefined(() -> {
+        return new NameToBeDefined2<V>(() -> {
             int retry = -1;
             EdsdkErrorException lastException;
             do {
                 try {
-                    this.runnable.run();
-                    return;
+                    return this.callable.call();
                 } catch (EdsdkErrorException e) {
                     lastException = e;
                     if (errors.contains(e.getEdsdkError())) {
                         if (sleep(delayMillis))
-                            return;
+                            return null;
                         retry++;
                     } else {
                         throw e;
@@ -116,23 +116,23 @@ public class NameToBeDefined implements Runnable {
         });
     }
 
-    public NameToBeDefined runOnError(final Runnable runOnError, final EdsdkError error) {
+    public NameToBeDefined2<V> runOnError(final Callable<V> runOnError, final EdsdkError error) {
         // can be called many times to add different handlers
         return runOnError(runOnError, Collections.singletonList(error));
     }
 
-    public NameToBeDefined runOnError(final Runnable runOnError, final EdsdkError... errors) {
+    public NameToBeDefined2<V> runOnError(final Callable<V> runOnError, final EdsdkError... errors) {
         return runOnError(runOnError, Arrays.asList(errors));
     }
 
-    public NameToBeDefined runOnError(final Runnable runOnError, final List<EdsdkError> errors) {
+    public NameToBeDefined2<V> runOnError(final Callable<V> runOnError, final List<EdsdkError> errors) {
         // can be called many times to add different handlers
-        return new NameToBeDefined(() -> {
+        return new NameToBeDefined2<V>(() -> {
             try {
-                this.runnable.run();
+                return this.callable.call();
             } catch (EdsdkErrorException e) {
                 if (errors.contains(e.getEdsdkError())) {
-                    runOnError.run();
+                    return runOnError.call();
                 } else {
                     throw e;
                 }
@@ -141,25 +141,32 @@ public class NameToBeDefined implements Runnable {
     }
 
     @Override
-    public void run() {
-        runnable.run();
+    public V call() throws Exception {
+        return callable.call();
     }
 
     public void handle(Consumer<Throwable> consumer) {
         try {
-            runnable.run();
+            callable.call();
         } catch (Throwable t) {
             consumer.accept(t);
         }
     }
 
-    public <R> R handle(Function<Throwable, R> function) {
+    public V handle(Function<Throwable, V> function) {
         try {
-            runnable.run();
+            return callable.call();
         } catch (Throwable t) {
             return function.apply(t);
         }
-        return null;
+    }
+
+    public <R> R handle(BiFunction<V, Throwable, R> function) {
+        try {
+            return function.apply(callable.call(), null);
+        } catch (Throwable t) {
+            return function.apply(null, t);
+        }
     }
 
     private boolean sleep(final long millis) {
