946348ee8fe9bad51d4899dc593c7d44638c9057
Blackdread <jojo.julien@gmail.com>
2018-12-09 18:22:50 +0800

    Add live view commands

--- camera-framework/src/main/java/org/blackdread/cameraframework/api/camera/CanonCamera.java
+++ camera-framework/src/main/java/org/blackdread/cameraframework/api/camera/CanonCamera.java
@@ -1,11 +1,9 @@
 package org.blackdread.cameraframework.api.camera;
 
-import org.blackdread.camerabinding.jna.EdsdkLibrary;
 import org.blackdread.camerabinding.jna.EdsdkLibrary.EdsCameraRef;
-import org.blackdread.camerabinding.jna.EdsdkLibrary.EdsObjectEventHandler;
-import org.blackdread.camerabinding.jna.EdsdkLibrary.EdsPropertyEventHandler;
 import org.blackdread.cameraframework.api.command.CanonCommand;
 import org.blackdread.cameraframework.api.command.GetPropertyCommand.ProductName;
+import org.blackdread.cameraframework.api.command.LiveViewCommand;
 import org.blackdread.cameraframework.api.command.SetPropertyCommand;
 import org.blackdread.cameraframework.api.command.ShootCommand;
 import org.blackdread.cameraframework.api.command.builder.ShootOption;
@@ -14,11 +12,13 @@ import org.blackdread.cameraframework.api.constant.EdsISOSpeed;
 import org.blackdread.cameraframework.api.helper.factory.CanonFactory;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 /**
+ * Give access to commands creation with simple API (only methods) with auto dispatch and options/decorators to be applied.
+ * <br>
+ * Most methods are async, few are sync but only for most used ones, reason is to not duplicate too much methods and reduce boiler-plate code of sync methods of declaring checked exceptions.
  * <p>Created on 2018/11/01.</p>
  *
  * @author Yoann CAPLAIN
@@ -27,12 +27,6 @@ public class CanonCamera {
 
     private EdsCameraRef cameraRef;
 
-    private final List<EdsdkLibrary.EdsStateEventHandler> stateEventHandlers = new ArrayList<>();
-
-    private final List<EdsPropertyEventHandler> propertyEventHandlers = new ArrayList<>();
-
-    private final List<EdsObjectEventHandler> objectEventHandlers = new ArrayList<>();
-
     /**
      * Default decorator builder for all commands created by this canon camera.
      * If null then is not applied.
@@ -40,6 +34,8 @@ public class CanonCamera {
     private CommandBuilderReusable commandBuilderReusable;
 
     private final Shoot shoot = new Shoot();
+
+    private final LiveView liveView = new LiveView();
 
     private final Property property = new Property();
 
@@ -52,7 +48,7 @@ public class CanonCamera {
 
     protected <T extends CanonCommand<R>, R> T applyExtraOptions(final T command) {
 
-        return null;
+        return command;
     }
 
     @SuppressWarnings("unchecked")
@@ -64,15 +60,36 @@ public class CanonCamera {
         return command;
     }
 
-    protected <T extends CanonCommand<R>, R> T dispatchCommand(T command) {
+    /**
+     * Dispatch a command with this camera as the owner and applies options/decorators of this camera.
+     *
+     * @param command command to dispatch, options and decorators maybe be applied to it before dispatched
+     * @param <T>     command type
+     * @param <R>     command return type
+     * @return command that was dispatched and which has been applied to it options and decorators
+     */
+    public <T extends CanonCommand<R>, R> T dispatchCommand(T command) {
         command = applyTarget(command);
+        command = applyExtraOptions(command);
         command = applyDefaultCommandDecoration(command);
         CanonFactory.commandDispatcher().scheduleCommand(cameraRef, command);
         return command;
     }
 
+    public CommandBuilderReusable getCommandBuilderReusable() {
+        return commandBuilderReusable;
+    }
+
+    public void setCommandBuilderReusable(final CommandBuilderReusable commandBuilderReusable) {
+        this.commandBuilderReusable = commandBuilderReusable;
+    }
+
     public Shoot getShoot() {
         return shoot;
+    }
+
+    public LiveView getLiveView() {
+        return liveView;
     }
 
     public Property getProperty() {
@@ -89,8 +106,40 @@ public class CanonCamera {
             return dispatchCommand(new ShootCommand());
         }
 
+        public List<File> shoot(final ShootOption shootOption) throws ExecutionException, InterruptedException {
+            return dispatchCommand(new ShootCommand(shootOption)).get();
+        }
+
         public ShootCommand shootAsync(final ShootOption shootOption) {
             return dispatchCommand(new ShootCommand(shootOption));
+        }
+
+    }
+
+    public final class LiveView {
+
+        public LiveViewCommand.Begin beginLiveViewAsync() {
+            return dispatchCommand(new LiveViewCommand.Begin());
+        }
+
+        public LiveViewCommand.End endLiveViewAsync() {
+            return dispatchCommand(new LiveViewCommand.End());
+        }
+
+        public LiveViewCommand.Download downloadLiveViewAsync() {
+            return dispatchCommand(new LiveViewCommand.Download());
+        }
+
+        public LiveViewCommand.DownloadBuffer downloadBufferLiveViewAsync() {
+            return dispatchCommand(new LiveViewCommand.DownloadBuffer());
+        }
+
+        public LiveViewCommand.IsLiveViewEnabled isLiveViewEnabledAsync() {
+            return dispatchCommand(new LiveViewCommand.IsLiveViewEnabled());
+        }
+
+        public LiveViewCommand.IsLiveViewActive isLiveViewActiveAsync() {
+            return dispatchCommand(new LiveViewCommand.IsLiveViewActive());
         }
 
     }
--- camera-framework/src/main/java/org/blackdread/cameraframework/api/command/LiveViewCommand.java
+++ camera-framework/src/main/java/org/blackdread/cameraframework/api/command/LiveViewCommand.java
@@ -1,5 +1,11 @@
 package org.blackdread.cameraframework.api.command;
 
+import org.blackdread.camerabinding.jna.EdsdkLibrary;
+
+import java.awt.image.BufferedImage;
+
+import static org.blackdread.cameraframework.api.helper.factory.CanonFactory.liveViewLogic;
+
 /**
  * <p>Created on 2018/10/02.<p>
  *
@@ -12,5 +18,98 @@ public abstract class LiveViewCommand<R> extends AbstractCanonCommand<R> {
 
     public LiveViewCommand(final LiveViewCommand toCopy) {
         super(toCopy);
+    }
+
+
+    public static class Begin extends LiveViewCommand<Void> {
+
+        public Begin() {
+        }
+
+        public Begin(final Begin toCopy) {
+            super(toCopy);
+        }
+
+        @Override
+        protected Void runInternal() {
+            liveViewLogic().beginLiveView((EdsdkLibrary.EdsCameraRef) getTargetRefInternal());
+            return null;
+        }
+    }
+
+    public static class End extends LiveViewCommand<Void> {
+
+        public End() {
+        }
+
+        public End(final End toCopy) {
+            super(toCopy);
+        }
+
+        @Override
+        protected Void runInternal() {
+            liveViewLogic().endLiveView((EdsdkLibrary.EdsCameraRef) getTargetRefInternal());
+            return null;
+        }
+    }
+
+    public static class Download extends LiveViewCommand<BufferedImage> {
+
+        public Download() {
+        }
+
+        public Download(final Download toCopy) {
+            super(toCopy);
+        }
+
+        @Override
+        protected BufferedImage runInternal() {
+            return liveViewLogic().getLiveViewImage((EdsdkLibrary.EdsCameraRef) getTargetRefInternal());
+        }
+    }
+
+    public static class DownloadBuffer extends LiveViewCommand<byte[]> {
+
+        public DownloadBuffer() {
+        }
+
+        public DownloadBuffer(final DownloadBuffer toCopy) {
+            super(toCopy);
+        }
+
+        @Override
+        protected byte[] runInternal() {
+            return liveViewLogic().getLiveViewImageBuffer((EdsdkLibrary.EdsCameraRef) getTargetRefInternal());
+        }
+    }
+
+    public static class IsLiveViewEnabled extends LiveViewCommand<Boolean> {
+
+        public IsLiveViewEnabled() {
+        }
+
+        public IsLiveViewEnabled(final IsLiveViewEnabled toCopy) {
+            super(toCopy);
+        }
+
+        @Override
+        protected Boolean runInternal() {
+            return liveViewLogic().isLiveViewEnabled((EdsdkLibrary.EdsCameraRef) getTargetRefInternal());
+        }
+    }
+
+    public static class IsLiveViewActive extends LiveViewCommand<Boolean> {
+
+        public IsLiveViewActive() {
+        }
+
+        public IsLiveViewActive(final IsLiveViewActive toCopy) {
+            super(toCopy);
+        }
+
+        @Override
+        protected Boolean runInternal() {
+            return liveViewLogic().isLiveViewEnabledByDownloadingOneImage((EdsdkLibrary.EdsCameraRef) getTargetRefInternal());
+        }
     }
 }
