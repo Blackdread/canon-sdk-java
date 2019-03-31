942941e31254b31c0c710ef32964bc1413a1c6e1
Blackdread <jojo.julien@gmail.com>
2019-03-31 16:33:14 +0800

    desc logic test

--- camera-framework/src/test/java/org/blackdread/cameraframework/api/helper/factory/PropertyDescLogicDefaultMockTest.java
+++ camera-framework/src/test/java/org/blackdread/cameraframework/api/helper/factory/PropertyDescLogicDefaultMockTest.java
@@ -5,17 +5,25 @@ import org.blackdread.camerabinding.jna.EdsPropertyDesc;
 import org.blackdread.camerabinding.jna.EdsdkLibrary.EdsCameraRef;
 import org.blackdread.cameraframework.AbstractMockTest;
 import org.blackdread.cameraframework.MockFactory;
-import org.blackdread.cameraframework.api.constant.EdsISOSpeed;
-import org.blackdread.cameraframework.api.constant.EdsPropertyID;
-import org.blackdread.cameraframework.api.constant.NativeEnum;
+import org.blackdread.cameraframework.api.constant.*;
 import org.blackdread.cameraframework.api.helper.logic.PropertyDescLogic;
+import org.blackdread.cameraframework.exception.error.device.EdsdkDeviceInvalidErrorException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.of;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 /**
  * <p>Created on 2019/03/31.</p>
@@ -44,6 +52,47 @@ class PropertyDescLogicDefaultMockTest extends AbstractMockTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    private static Stream<Arguments> compatibleNatives() {
+        return Stream.of(
+            of(EdsPropertyID.kEdsPropID_AEMode, EdsAEMode.kEdsAEMode_CandlelightPortraits, EdsAEMode.kEdsAEMode_Bulb),
+            of(EdsPropertyID.kEdsPropID_AEModeSelect, EdsAEModeSelect.kEdsAEModeSelect_Custom1, EdsAEModeSelect.kEdsAEModeSelect_Custom2),
+            of(EdsPropertyID.kEdsPropID_ISOSpeed, EdsISOSpeed.kEdsISOSpeed_50, EdsISOSpeed.kEdsISOSpeed_100),
+            of(EdsPropertyID.kEdsPropID_MeteringMode, EdsMeteringMode.kEdsMeteringMode_Evaluative, EdsMeteringMode.kEdsMeteringMode_Spot),
+            of(EdsPropertyID.kEdsPropID_Av, EdsAv.kEdsAv_3_2, EdsAv.kEdsAv_2_5),
+            of(EdsPropertyID.kEdsPropID_Tv, EdsTv.kEdsTv_0_8, EdsTv.kEdsTv_1),
+            of(EdsPropertyID.kEdsPropID_ExposureCompensation, EdsExposureCompensation.kEdsExposureCompensation_1_1by3, EdsExposureCompensation.kEdsExposureCompensation_3_2by3),
+            of(EdsPropertyID.kEdsPropID_ImageQuality, EdsImageQuality.EdsImageQuality_CRLJ, EdsImageQuality.EdsImageQuality_CRLJF),
+            of(EdsPropertyID.kEdsPropID_WhiteBalance, EdsWhiteBalance.kEdsWhiteBalance_Auto, EdsWhiteBalance.kEdsWhiteBalance_Strobe),
+            of(EdsPropertyID.kEdsPropID_PictureStyle, EdsPictureStyle.kEdsPictureStyle_Monochrome, EdsPictureStyle.kEdsPictureStyle_User1),
+            of(EdsPropertyID.kEdsPropID_DriveMode, EdsDriveMode.kEdsDriveMode_14FpsSuperHighSpeed, EdsDriveMode.kEdsDriveMode_SilentSingleShooting),
+            of(EdsPropertyID.kEdsPropID_Evf_WhiteBalance, EdsWhiteBalance.kEdsWhiteBalance_AwbWhite, EdsWhiteBalance.kEdsWhiteBalance_PCSet2),
+            of(EdsPropertyID.kEdsPropID_Evf_AFMode, EdsEvfAFMode.Evf_AFMode_LiveFace, EdsEvfAFMode.Evf_AFMode_LiveZone)
+        );
+    }
+
+
+    @MethodSource("compatibleNatives")
+    @ParameterizedTest
+    void getPropertyDescCompatibleNative(final EdsPropertyID propertyID, final NativeEnum<Integer> v1, final NativeEnum<Integer> v2) {
+        final NativeLong[] desc = new NativeLong[128];
+        desc[0] = new NativeLong(v1.value());
+        desc[1] = new NativeLong(v2.value());
+        final EdsPropertyDesc propertyDesc = new EdsPropertyDesc(
+            new NativeLong(0),
+            new NativeLong(0),
+            new NativeLong(2),
+            desc
+        );
+
+        propertyDescLogicDefaultExtended.setPropertyDescStructure(propertyDesc);
+
+        final List<NativeEnum<Integer>> nativeEnums = propertyDescLogicDefaultExtended.getPropertyDesc(fakeCamera, propertyID);
+
+        Assertions.assertEquals(2, nativeEnums.size());
+        Assertions.assertEquals(v1, nativeEnums.get(0));
+        Assertions.assertEquals(v2, nativeEnums.get(1));
     }
 
     @Test
@@ -135,14 +184,19 @@ class PropertyDescLogicDefaultMockTest extends AbstractMockTest {
 
     @Test
     void getPropertyDescStructure() {
+        when(CanonFactory.edsdkLibrary().EdsGetPropertyDesc(eq(fakeCamera), eq(new NativeLong(EdsPropertyID.kEdsPropID_ISOSpeed.value())), any())).thenReturn(new NativeLong(0));
+
+        final EdsPropertyDesc propertyDesc = spyPropertyDescLogic.getPropertyDescStructure(fakeCamera, EdsPropertyID.kEdsPropID_ISOSpeed);
+
+        Assertions.assertNotNull(propertyDesc);
     }
 
+    @Test
+    void getPropertyDescStructureThrowsOnError() {
+        when(CanonFactory.edsdkLibrary().EdsGetPropertyDesc(eq(fakeCamera), eq(new NativeLong(EdsPropertyID.kEdsPropID_ISOSpeed.value())), any())).thenReturn(new NativeLong(EdsdkError.EDS_ERR_DEVICE_INVALID.value()));
 
-//        when(CanonFactory.getCanonFactory().getPropertyDescLogic()).thenReturn(propertyDescLogicDefaultExtended);
+        Assertions.assertThrows(EdsdkDeviceInvalidErrorException.class, () -> spyPropertyDescLogic.getPropertyDescStructure(fakeCamera, EdsPropertyID.kEdsPropID_ISOSpeed));
 
-//        when(propertyDescLogic.getPropertyDescStructure(any(), any())).thenReturn(propertyDesc);
-
-//        final List<Integer> propertyDescColorTemperature = spyPropertyDescLogic.getPropertyDescColorTemperature(fakeCamera);
-//        final List<Integer> propertyDescColorTemperature = CanonFactory.getCanonFactory().getPropertyDescLogic().getPropertyDescColorTemperature(fakeCamera);
+    }
 
 }
